93b1dadd4cea8770b389e570bf10498739bc27d5
tpadgett <tim_padgett@yahoo.com>
2021-04-28 16:02:40 -0700

    glass visual one

--- scripts/glassGraphics.js
+++ scripts/glassGraphics.js
@@ -43,11 +43,11 @@ container.addEventListener('click', function(){
   analyser = audioCtx.createAnalyser();
   audioSource.connect(analyser);
   analyser.connect(audioCtx.destination);
-  analyser.fftSize = 64;
+  analyser.fftSize = 512;
   const bufferLength = analyser.frequencyBinCount;
 
   const dataArray = new Uint8Array(bufferLength);
-  const barWidth = canvas.width/bufferLength;
+  const barWidth = (canvas.width/bufferLength) / 2;
   let barHeight;
   let x;
 
@@ -55,15 +55,38 @@ container.addEventListener('click', function(){
     x = 0;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     analyser.getByteFrequencyData(dataArray);
-    for (let i = 0; i < bufferLength; i++) {
-      barHeight = dataArray[i];
-      ctx.fillStyle = 'white';
-      ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-      x += barWidth;
-    }
+    drawGlass(bufferLength, x, barWidth, barHeight, dataArray);
+
     requestAnimationFrame(animate);
   }
   animate();
   }
-})
+});
+
+function drawGlass(bufferLength, x, barWidth, barHeight, dataArray) {
+  for (let i = 0; i < bufferLength; i++) {
+    barHeight = dataArray[i] * 1.5;
+    ctx.save();
+    ctx.translate(canvas.width/2, canvas.height/2);
+    ctx.rotate(i * Math.PI * 4 / bufferLength);
+    const hue = i * 15;
+
+    ctx.fillStyle = 'hsl(' + hue + ',100%, 50%)';
+    ctx.fillRect(0, 0, barWidth, barHeight);
+    x += barWidth;
+    ctx.restore();
+  }
+  // for (let i = 0; i < bufferLength; i++) {
+  //   barHeight = dataArray[i] * 1.5;
+  //   ctx.save();
+  //   ctx.translate(canvas.width / 2, canvas.height / 2);
+  //   ctx.rotate(i * Math.PI / bufferLength);
+  //   const hue = i * 15;
+
+  //   ctx.fillStyle = 'hsl(' + hue + ',100%, 50%)';
+  //   ctx.fillRect(0, 0, barWidth, barHeight);
+  //   x += barWidth;
+  //   ctx.restore();
+  // }
+}
 
