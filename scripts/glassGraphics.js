ee9154999d91b2f82c4b65f3e1782ed92096242a
tpadgett <tim_padgett@yahoo.com>
2021-04-30 12:31:56 -0700

    full size background

--- scripts/glassGraphics.js
+++ scripts/glassGraphics.js
@@ -93,33 +93,6 @@ function drawGlass(bufferLength, x, barWidth, barHeight, dataArray) {
     
     x += barWidth;
     ctx.restore();
-
-
   }
-
-
-
-
-
-
-
-  // for (let i = 0; i < bufferLength; i++) {
-  //   barHeight = dataArray[i] * 2.3;
-  //   const red = i * barHeight/2;
-  //   const green = i * 10;
-  //   const blue = barHeight / 1.3;
-  //   ctx.fillStyle = 'rgb(' + red + ',' + green + ',' + blue + ')';
-  //   ggCtx.fillRect(canvas.width / 2 - x, canvas.height - barHeight, barWidth, barHeight);
-  //   x += barWidth;
-  // }
-  // for (let i = 0; i < bufferLength; i++) {
-  //   barHeight = dataArray[i] * 2.3;
-  //   const red = i * barHeight/2 ;
-  //   const green = i * 8;
-  //   const blue = barHeight / 1.7;
-  //   ctx.fillStyle = 'rgb(' + red + ',' + green + ',' + blue + ')';
-  //   ggCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-  //   x += barWidth;
-  // }
 }
 
